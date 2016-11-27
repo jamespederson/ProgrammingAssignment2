c253fba92a6c7cb44692deaf8dda4f28c69f16c2
James Pederson <James Pederson>
2016-11-27 17:53:35 -0500

    Committer: James Pederson <James Pederson> On branch master Your branch is up-to-date with 'origin/master'. Changes to be committed: 	modified:   cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R-Programming assignment #2.
+## The following functions will demonstrate a working knowledge of the lexical
+##    scoping concept within the R-Programming language.
+## Written by: James Pederson
 
-## Write a short comment describing this function
+## This function will create a special matrix object whose inverse can be cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function will compute the inverse of the makeCacheMatrix object above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
